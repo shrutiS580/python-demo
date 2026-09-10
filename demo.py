7b7e3f2b1620aba46b4399e12faff696246181fc
shruti swami <shrutiswami791@gmail.com>
2026-09-10 13:54:47 +0530

    my python project

---
+++ demo.py
@@ -0,0 +1,7 @@
+role = str(input("enter ypur role"))
+age=int(input("enter your age"))
+Eligble= age>=21 and role =="student"
+print("eligble :",True)
+#write a program to swap two variables without a third variable using arith,matic operation 
+#eg : before swap a=10; b=20
+#after swap a=20,b=10
